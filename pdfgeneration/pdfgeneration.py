ffa5980e57fd1a3eed21b5dfc0104b921ac3b473
matthew.wang@darwinai.ca <mslwang2001@gmail.com>
2021-07-14 17:46:56 -0400

    Change decimals to percentages with 2 digits of precision

--- pdfgeneration/pdfgeneration.py
+++ pdfgeneration/pdfgeneration.py
@@ -179,7 +179,7 @@ class Pdfgeneration(ChrisApp):
             for column_name in classification_data:
                 prediction = classification_data.get(column_name, 'N/A')
                 if (column_name != 'prediction') and (column_name != 'Prediction') and (column_name != '**DISCLAIMER**'):
-                    prediction_analysis += f"<h3>{column_name}: <span>{prediction}</span></h3>"
+                    prediction_analysis += f"<h3>{column_name}: <span>{float(prediction)*100:.2f}%</span></h3>"
 
             txt = txt.replace("${PRED_ANALYSIS}", prediction_analysis)
             txt = txt.replace("${X-RAY-IMAGE}", options.imagefile)
